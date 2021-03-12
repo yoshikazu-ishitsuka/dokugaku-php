eb81899dd71d314636f902b5dab8e60e3890a600
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-12 16:00:29 +0900

    フォームの練習用ファイルの作成

---
+++ src/tmp/test.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<form action="create.php" method="post">
+    <div>
+        <input type="radio" name="sex" id="male" value="male">
+        <label for="male">男性</label>
+    </div>
+    <div>
+        <input type="radio" name="sex" id="female" value="female">
+        <label for="female">女性</label>
+    </div>
+    <button type="submit">登録する</button>
+</form>
