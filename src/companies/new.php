a58fe1b39de3808eeb0520d4d886d4b6da9e6b65
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-13 09:20:09 +0900

    update new.php

--- src/companies/new.php
+++ src/companies/new.php
@@ -11,7 +11,22 @@
 </head>
 
 <body>
-    <p>nnnn</p>
+    <h1>会社情報の登録</h1>
+    <form action="create.php" method="post">
+        <div>
+            <label for="name">会社名</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div>
+            <label for="establishment_date">設立日</label>
+            <input type="date" name="establishment_date" id="establishment_date">
+        </div>
+        <div>
+            <label for="founder">代表者</label>
+            <input type="text" name="founder" id="founder">
+        </div>
+        <button type="submit">登録する</button>
+    </form>
 </body>
 
 </html>
