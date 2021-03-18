c70cd51346b76d7525580fe1bda3c7a358ee7c3a
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-18 11:56:42 +0900

    update create.php

--- src/companies/create.php
+++ src/companies/create.php
@@ -83,47 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // var_export(100);
         header("Location: index.php");
     }
-
-    // もしエラーがあれば下記
-
 }
 
-?>
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会社情報の登録</title>
-</head>
-
-<body>
-    <h1>会社情報の登録</h1>
-    <form action="create.php" method="post">
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-
-        <div>
-            <label for="name">会社名</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div>
-            <label for="establishment_date">設立日</label>
-            <input type="date" name="establishment_date" id="establishment_date">
-        </div>
-        <div>
-            <label for="founder">代表者</label>
-            <input type="text" name="founder" id="founder">
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+// もしエラーがあれば下記
+include 'views/new.php';
