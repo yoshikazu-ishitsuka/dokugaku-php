64e90720d14eea94d024275fef24fa6847ef4645
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-18 11:57:09 +0900

    update new.php

--- src/companies/new.php
+++ src/companies/new.php
@@ -1,32 +1,11 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="ja">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会社情報の登録</title>
-</head>
+$company = [
+    'name' => '',
+    'establishment_date' => '',
+    'founder' => ''
+];
+// $errorsを定義しておけばHTMLのエラーが起きない
+$errors = [];
 
-<body>
-    <h1>会社情報の登録</h1>
-    <form action="create.php" method="post">
-        <div>
-            <label for="name">会社名</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div>
-            <label for="establishment_date">設立日</label>
-            <input type="date" name="establishment_date" id="establishment_date">
-        </div>
-        <div>
-            <label for="founder">代表者</label>
-            <input type="text" name="founder" id="founder">
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+include 'views/new.php';
