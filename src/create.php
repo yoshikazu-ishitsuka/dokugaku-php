4b3ba2922731307cd9697138eaf17f66efd09878
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-18 14:23:34 +0900

    update: src/create.php

--- src/create.php
+++ src/create.php
@@ -55,9 +55,14 @@ function validate($review)
     // 評価
     if (!strlen($review['score'])) {
         $errors['score'] = '評価を入力してください';
-    } elseif (!in_array($review['score'], [1, 2, 3, 4, 5], true)) {
+    } elseif (!in_array($review['score'], ["1", "2", "3", "4", "5"], true)) {
         $errors['score'] = '評価は1~5の数字で入力してください';
     }
+
+    // // 評価の別パターン
+    // if ($review['score'] < 1 || $review['score'] > 5) {
+    //     $errors['score'] = '評価は1~5の整数で入力してください';
+    // }
 
     // 感想
     if (!strlen($review['summary'])) {
@@ -105,69 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // リダイレクトする
         header("Location: index.php");
     }
-    // もしエラーがあれば下記
-
-
 }
 
-?>
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>読書ログの登録</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <form action="create.php" method="post">
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-
-        <h3>読書ログの登録</h3>
-        <div>
-            <label for="title">書籍名</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" name="author" id="author">
-        </div>
-        <div>
-            <label>読書状況</label>
-            <div>
-                <div>
-                    <input type="radio" name="status" id="status1" value="未読">
-                    <label for="status1">未読</label>
-                </div>
-                <div>
-                    <input type="radio" name="status" id="status2" value="読んでる">
-                    <label for="status2">読んでる</label>
-                </div>
-                <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
-                    <label for="status3">読了</label>
-                </div>
-            </div>
-        </div>
-        <div>
-            <label for="score">評価(5点満点の整数)</label>
-            <input type="number" name="score" id="score">
-        </div>
-        <div>
-            <label for="summary">感想</label>
-            <textarea type="text" name="summary" id="summary" rows="10"></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+// もしエラーがあれば下記
+include 'views/new.php';
