e9185ce44eb7ef276fe992aa3cdf8742969b92bf
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2021-03-19 16:17:39 +0900

    update: new.php

--- src/companies/new.php
+++ src/companies/new.php
@@ -8,4 +8,7 @@ $company = [
 // $errorsを定義しておけばHTMLのエラーが起きない
 $errors = [];
 
-include 'views/new.php';
+$title = '会社情報の登録';
+$content = __DIR__ . '/views/new.php';
+// 絶対パスに変更
+include __DIR__ . '/views/layout.php';
